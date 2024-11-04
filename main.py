9187d601f8d22c5bdbc977c10cee0a2213b484d3
destep <tankt34.stepanoff@yandex.ru>
2024-11-04 09:57:27 +0300

    change code

--- main.py
+++ main.py
@@ -2,25 +2,30 @@ from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "Добро пожаловать в API калькулятора"}
+
 
 @app.get("/add")
 async def add(x: float, y: float):
     return {"result": x + y}
 
+
 @app.get("/subtract")
 async def subtract(x: float, y: float):
     return {"result": x - y}
 
+
 @app.get("/multiply")
 def multiply(a: float, b: float):
     return {"result": a * b}
+
 
 @app.get("/divide")
 def divide(a: float, b: float):
     if b == 0:
         raise HTTPException(status_code=400, detail="Деление на ноль запрещено")
     return {"result": a / b}
-#бу
+# бу
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -7,10 +7,12 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 client = TestClient(app)
 
+
 def test_addition():
     response = client.get("/add?x=1.5&y=2.5")  # Используем float значения
     assert response.status_code == 200  # nosec
     assert response.json() == {"result": 4.0}  # nosec
+
 
 def test_subtraction():
     response = client.get("/subtract?x=5.0&y=3.0")  # Используем float значения
